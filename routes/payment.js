fc187ec8518e186ca356e824647a6ba0752140d2
NiceDayZ <craciunmihai40@gmail.com>
2020-05-11 23:02:41 +0300

    oops

--- routes/payment.js
+++ routes/payment.js
@@ -3,9 +3,9 @@ const router = require("express").Router();
 const { paymentController } = require("../controllers");
 const verifyToken = require("./verifyToken");
 
-router.post('/intend', verifyToken, paymentController.getPaymentIntend); //to add verifyToken
+router.post('/intend', verifyToken, paymentController.getPaymentIntend); 
 router.get('/success', paymentController.getSuccessPayment);
-router.get('cancel', paymentController.getCancelledPayment);
+router.get('/cancel', paymentController.getCancelledPayment);
 router.get('/payouts', verifyToken, paymentController.getAllPaymentsToUser);
 router.get('/payments', verifyToken, paymentController.getAllPaymentsFromAUser);
 
